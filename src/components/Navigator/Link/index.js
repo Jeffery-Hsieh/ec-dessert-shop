671cec6aba704e322cf819e19d061170fb1b946d
Jeffery-Hsieh <jeffery85616@gmail.com>
2020-10-09 00:26:20 +0800

    Change <a> tag default css

--- src/components/Navigator/Link/index.js
+++ src/components/Navigator/Link/index.js
@@ -2,11 +2,18 @@ import React from "react";
 import { Link as RouterLink } from "react-router-dom";
 import "./style.scss";
 
-const Link = ({ href, en, locale }) => (
-  <RouterLink className="link" to={`${href}`}>
-    <div className="link--zhText">{locale}</div>
-    <div className="link--enText">{en}</div>
-  </RouterLink>
-);
+const Link = ({ href, en, locale, disabled }) => {
+  let linkClassName = "link";
+  if (disabled) {
+    linkClassName += " disabled-link";
+  }
+
+  return (
+    <RouterLink className={linkClassName} to={`${href}`}>
+      <div className="link--zhText">{locale}</div>
+      <div className="link--enText">{en}</div>
+    </RouterLink>
+  );
+};
 
 export default Link;
--- src/components/Navigator/index.js
+++ src/components/Navigator/index.js
@@ -1,27 +1,38 @@
 import React from "react";
-import LinkWithIcon from "./LinkWithIcon";
 import Link from "./Link";
+import { Link as RouterLink } from "react-router-dom";
+import LinkWithIcon from "./LinkWithIcon";
 import "./style.scss";
 
 const Navigator = () => {
   const links = [
-    { en: "Gallery", locale: "商品集", href: "/gallery" },
-    { en: "Online Shop", locale: "線上商店", href: "/onlineShop" },
-    { en: "Authorized Store", locale: "合作商家", href: "authorizedStore" },
-    { en: "Contact us", locale: "聯絡我們", href: "/contact" },
+    { en: "Gallery", locale: "商品集", href: "/gallery", disabled: true },
+    {
+      en: "Online Shop",
+      locale: "線上商店",
+      href: "/onlineShop",
+      disabled: false,
+    },
+    {
+      en: "Authorized Store",
+      locale: "合作商家",
+      href: "authorizedStore",
+      disabled: false,
+    },
+    { en: "Contact us", locale: "聯絡我們", href: "/contact", disabled: true },
   ];
 
-  const navigatorLinks = links.map((link) => (
-    <Link className="navigator__link" {...link} />
-  ));
+  const navigatorLinks = links.map((link) => <Link {...link} />);
 
   return (
     <div className="navigator">
-      <img
-        src={require("../../Assets/logo.png")}
-        className="navigator__logo"
-        alt="Beigo"
-      />
+      <RouterLink to="/">
+        <img
+          src={require("../../Assets/logo.png")}
+          className="navigator__logo"
+          alt="Beigo"
+        />
+      </RouterLink>
       <div className="d-flex flex-column justify-content-around navigator__link">
         {navigatorLinks}
         <LinkWithIcon />
